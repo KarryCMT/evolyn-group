568fd7f0697f65b02ccfe04037fa1107de397f0f
karrylee <karrylee@msi.com>
2026-08-17 16:54:10 +0800

    Add: 优化

--- apps/evolyn-web/apps/web/src/main.ts
+++ apps/evolyn-web/apps/web/src/main.ts
@@ -14,7 +14,6 @@ import App from './App.vue'
 
 import '~/styles/index.scss'
 
-import 'uno.css'
 // If you want to use ElMessage, import it.
 import 'element-plus/theme-chalk/src/message.scss'
 import 'element-plus/theme-chalk/src/message-box.scss'
