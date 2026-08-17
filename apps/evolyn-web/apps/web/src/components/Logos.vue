52885bdac633b2aac1aba312b80e2f478b395d98
karrylee <karrylee@msi.com>
2026-08-17 16:49:54 +0800

    Add: file

---
+++ apps/evolyn-web/apps/web/src/components/Logos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+    </a>
+    <a href="https://element-plus.org/" target="_blank">
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.logo.element-plus:hover {
+  filter: drop-shadow(0 0 2em #409effaa);
+}
+</style>
